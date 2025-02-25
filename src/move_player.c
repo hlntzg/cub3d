fd325721ccc832a9cc8cebecf5d500cf762c8abd
Juliette Mouette <jmouette@c1r1p2.hive.fi>
2025-02-25 15:13:30 +0200

    move left/right ok

--- src/move_player.c
+++ src/move_player.c
@@ -6,84 +6,79 @@
 /*   By: jmouette <jmouette@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 16:47:33 by jmouette          #+#    #+#             */
-/*   Updated: 2025/02/19 17:03:43 by jmouette         ###   ########.fr       */
+/*   Updated: 2025/02/25 15:11:07 by jmouette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3D.h"
 
-void	move_player_left(t_game *game)
-{
-	double		new_x;
-	double		new_y;
-	char		**map;
-	t_coordd	*p;
-	t_raycast	*ray;
-
-	map = game->data->map;
-	p = &game->player->position;
-	ray = game->ray;
-	new_x = p->x - ray->direction.x * MOVE_SPEED;
-	new_y = p->y + ray->direction.y * MOVE_SPEED;
-	if (map[(int)p->y][(int)new_x] == '0')
-		p->x = new_x;
-	if (map[(int)new_y][(int)p->x] == '0')
-		p->y = new_y;
-}
-
-void	move_player_right(t_game *game)
-{
-	double		new_x;
-	double		new_y;
-	char		**map;
-	t_coordd	*p;
-	t_raycast	*ray;
-
-	map = game->data->map;
-	p = &game->player->position;
-	ray = game->ray;
-	new_x = p->x + ray->direction.x * MOVE_SPEED;
-	new_y = p->y - ray->direction.y * MOVE_SPEED;
-	if (map[(int)p->y][(int)new_x] == '0')
-		p->x = new_x;
-	if (map[(int)new_y][(int)p->x] == '0')
-		p->y = new_y;
-}
-
 void	move_player_forward(t_game *game)
 {
 	double		new_x;
 	double		new_y;
-	char		**map;
 	t_coordd	*p;
-	t_raycast	*ray;
 
-	map = game->data->map;
 	p = &game->player->position;
-	ray = game->ray;
-	new_x = p->x + ray->direction.x * MOVE_SPEED;
-	new_y = p->y + ray->direction.y * MOVE_SPEED;
-	if (map[(int)p->y][(int)new_x] == '0')
+
+	printf("position: %f %f\n", p->x, p->y);
+	new_x = p->x + game->player->d.x * MOVE_SPEED;
+	new_y = p->y + game->player->d.y * MOVE_SPEED;
+	if (game->data->map[(int)new_y][(int)new_x] != '1')
+	{
 		p->x = new_x;
-	if (map[(int)new_y][(int)p->x] == '0')
 		p->y = new_y;
+	}
+	printf("position: %f %f\n", p->x, p->y);
 }
 
 void	move_player_backward(t_game *game)
 {
 	double		new_x;
 	double		new_y;
-	char		**map;
 	t_coordd	*p;
-	t_raycast	*ray;
 
-	map = game->data->map;
 	p = &game->player->position;
-	ray = game->ray;
-	new_x = p->x - ray->direction.x * MOVE_SPEED;
-	new_y = p->y - ray->direction.y * MOVE_SPEED;
-	if (map[(int)p->y][(int)new_x] == '0')
+	new_x = p->x - game->player->d.x * MOVE_SPEED;
+	new_y = p->y - game->player->d.y * MOVE_SPEED;
+	if (game->data->map[(int)new_y][(int)new_x] != '1')
+	{
 		p->x = new_x;
-	if (map[(int)new_y][(int)p->x] == '0')
 		p->y = new_y;
+	}
+	printf("position: %f %f\n", p->x, p->y);
+}
+
+void	move_player_right(t_game *game)
+{
+	double		new_x;
+	double		new_y;
+	t_coordd	*p;
+
+	p = &game->player->position;
+	new_x = p->x - game->player->d.y * MOVE_SPEED;
+	new_y = p->y + game->player->d.x * MOVE_SPEED;
+	if (game->data->map[(int)new_y][(int)new_x] != '1')
+	{
+		p->x = new_x;
+		p->y = new_y;
+	}
+	printf("position: %f %f\n", p->x, p->y);
+}
+
+void	move_player_left(t_game *game)
+{
+	double		new_x;
+	double		new_y;
+	t_coordd	*p;
+
+	p = &game->player->position;
+
+	new_x = p->x + game->player->d.y * MOVE_SPEED;
+	new_y = p->y - game->player->d.x * MOVE_SPEED;
+	if (game->data->map[(int)new_y][(int)new_x] != '1')
+	{
+		p->x = new_x;
+		p->y = new_y;
+	}
+	printf("position: %f %f\n", p->x, p->y);
 }

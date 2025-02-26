b18f1666b9277e69c636e8e9f33ff6003ba5bac2
Juliette Mouette <jmouette@c1r5p9.hive.fi>
2025-02-26 13:56:05 +0200

    resizing window ok

--- src/main.c
+++ src/main.c
@@ -6,11 +6,35 @@
 /*   By: jmouette <jmouette@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 16:41:10 by jmouette          #+#    #+#             */
-/*   Updated: 2025/02/25 16:50:41 by jmouette         ###   ########.fr       */
+/*   Updated: 2025/02/26 13:54:20 by jmouette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3D.h"
+
+void	resize_window(int width, int height, void *param)
+{
+	int	i;
+	t_game	*game;
+
+	game = (t_game *)param;
+    game->win_w = width;
+    game->win_h = height;
+	if(game->render->pixels)
+		free_pixels(game->render->pixels);
+    game->render->pixels = ft_calloc(game->win_h + 1, sizeof(uint32_t *));
+	if (!game->render->pixels)
+		return (ft_putstr_fd("Memory allocation failed for pixels" , 2));
+	i = 0;
+	while (i < game->win_h)
+	{
+		game->render->pixels[i] = ft_calloc(game->win_w, sizeof(uint32_t));
+		if (!game->render->pixels[i])
+			return (ft_putstr_fd("Memory allocation failed for pixels row", 2));
+		i++;
+	}
+    return ;
+}
 
 int	start_game(t_game *game)
 {
@@ -24,6 +48,7 @@ int	start_game(t_game *game)
 	mlx_loop_hook(game->mlx, &rendering_game, game);
 	return (EXIT_SUCCESS);
 }
+
 
 int	main(int argc, char **argv)
 {
@@ -41,6 +66,7 @@ int	main(int argc, char **argv)
 		return (free_game(&game), EXIT_FAILURE);
 	if (start_game(&game) == 1)
 		return (exit_game(&game), EXIT_FAILURE);
+	mlx_resize_hook(game.mlx, resize_window, &game);
 	mlx_loop(game.mlx);
 	exit_game(&game);
 	return (EXIT_SUCCESS);
